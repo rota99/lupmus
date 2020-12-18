686a33a4ea4780d809ba0d04d7a35540bbfd627a
rota99 <35954528+rota99@users.noreply.github.com>
2020-12-18 14:03:39 +0100

    init

---
+++ Wherewolf/WherewolfTest.js
@@ -0,0 +1,60 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const dataservice = require("./function.js")
+const prefix = ".";
+
+client.on('ready', () => {
+  console.log(`Bro, io sottoscritto ${client.user.tag} sono pronto`);
+});
+
+
+client.on("message", function(message) {
+  if (message.author.bot) return;
+  if (!message.content.startsWith(prefix)) return;
+
+  const commandBody = message.content.slice(prefix.length);
+  const args = commandBody.split(' ');
+  const command = args.shift().toLowerCase();
+
+    const channel = message.channel
+    const members = channel.members
+
+      const fasi = message.content.slice(prefix.length).split(' ').shift().toLowerCase();
+        switch (fasi) {
+
+          case "clear":
+
+            dataservice.clear(message, args)
+
+            break;
+
+          case "notte":
+
+            dataservice.mute(members, message)
+
+            break;
+
+          case "accuse":
+
+            dataservice.unmute(members, message)
+            dataservice.timeraccuse(message)
+
+            break;
+
+          case "rogo":
+
+            message.channel.send('demoghe fogooo');
+
+            break;
+
+          default:
+
+          message.channel.send('svejate fora');
+
+          break;
+
+          }
+});
+
+
+client.login("Nzg4NDk1MDI0MjQwNDU5ODA2.X9kVNw.MRytYUYLSzdzavannXfkllHSEFw")
