e43a9080c09efb17ebcf14beef96e5e35aea5bea
Aizakku2996 <jacopo_bordi@hotmail.it>
2020-12-18 22:03:55 +0100

    cambiato token

--- Wherewolf/Wherewolf.js
+++ Wherewolf/Wherewolf.js
@@ -47,4 +47,4 @@ client.on("message", function(message) {
 });
 
 
-client.login("Nzg4NDk1MDI0MjQwNDU5ODA2.X9kVNw.MRytYUYLSzdzavannXfkllHSEFw");
+client.login("Nzg4NDk1MDI0MjQwNDU5ODA2.X9kVNw.GtxAq6LfUdnND9q6sM-ZouxFL10");
