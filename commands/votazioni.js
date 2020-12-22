00e62d41d65ae187c902e674247d3ce115de3d4c
rota99 <35954528+rota99@users.noreply.github.com>
2020-12-22 23:46:29 +0100

    numero di votanti corretto; bozza embed

--- commands/votazioni.js
+++ commands/votazioni.js
@@ -1,40 +1,29 @@
-/*const channels = ['788491738578288651']
-
-module.exports = {
-  name: "votazioni",
-  description: "comando per generare una votazione",
-
-  execute(client, message) {
-    client.on('message', (message) => {
-      const { channel, content } = message
-
-      if (!channels.includes(channel.id)) {
-        return
-      }
-
-      const eachLine = content.split('\n')
-
-      for (const line of eachLine) {
-        if (line.includes('=')) {
-          const split = line.split('=')
-          const emoji = split[0].trim()
-          message.react(emoji)
-        }
-      }
-    })
-  }
-}*/
 const idNarratore = "774699081620521000";
 const idMorto = '780154332934438942';
 const idBot1 = '788498166847766571';
 const idBot2= '787753336169299998';
+const Discord = require("discord.js");
+var arrayVotanti = []
 
-const conta = (members) => {
-  var i = -1;
-  members.forEach(member => {
-    console.log(member.user.username)
+const pollBallottaggio = (idCanaleGenerale) => {
+
+  const exampleEmbed = new Discord.MessageEmbed()
+    .setColor('#5c4545')
+    .setTitle('Chi vuoi mandare al ballottaggio?')
+    .setDescription(`Al ballottaggio ci vanno ${arrayVotanti.join(', ')}`)
+
+    idCanaleGenerale.send(exampleEmbed);
+}
+
+const conta = (membersOnline) => {
+  var i = 0;
+  arrayVotanti.length = 0
+  membersOnline.forEach(member => {
     if (member._roles.includes(idNarratore) || member._roles.includes(idMorto) || member._roles.includes(idBot1) || member._roles.includes(idBot2) || member._roles.length == 0) {
       i++
+    }
+    else {
+      arrayVotanti.push(member.user.username)
     }
   });
   return i;
@@ -45,23 +34,29 @@ module.exports = {
   description:"comando per votare",
 
   async execute(members, message, args, client) {
-    var numero = client.guilds.cache.get('774369837727350844').channels.cache.get('774710293363949618').members.size
+    //var numero = client.guilds.cache.get('774369837727350844').channels.cache.get('774710293363949618').members.size
+    var membersOnline = client.guilds.cache.get('774369837727350844').channels.cache.get('774710293363949618').members
+    var idCanaleGenerale = client.guilds.cache.get('774369837727350844').channels.cache.get('774369837727350846')
     var votanti = 0;
+    console.log(args)
+
     const promiseConta = new Promise((resolve, reject) => {
-      if(conta(members) >= 0) {
-        resolve(conta(members))
+      let contaMembri = conta(membersOnline);
+      if(contaMembri >= 0) {
+        resolve(contaMembri)
       } else {
         reject("Conta è < 0.")
       }
     });
 
     promiseConta.then((data) => {
-      votanti = numero - data;
-      console.log(votanti);
+      votanti = membersOnline.size - data;
+      message.channel.send(`Possono votare ${votanti} persone`);
+
+      if(args[0] == 'b')
+        pollBallottaggio(idCanaleGenerale)
     }).catch((error) => {
       console.log(error);
     });
-    /*var votanti = numero - await conta(members)
-    message.channel.send(numero + '-' + await conta(members) + '=' + votanti)*/
   }
 }
