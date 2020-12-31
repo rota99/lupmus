517bb8a38634a383a07d0a2afb0417040a683fb1
Aizakku2996 <jacopo_bordi@hotmail.it>
2020-12-31 21:04:49 +0100

    guardie e criminali

--- commands/criminali.js
+++ commands/criminali.js
@@ -1,3 +1,6 @@
+const canaliCriminali = ["786684145308663858", "787045057408663612", "787045076241088594", "787044819834241095"]
+const ruoliEsclusi = ['774699081620521000', '780154332934438942', '787753336169299998']
+
 module.exports = {
   name: "c",
   description: "Comando per riconoscere far riconoscere i criminali",
@@ -6,6 +9,7 @@ module.exports = {
     const idDiscussione = '774710293363949618';
     var numeroCriminali = 0;
     members = client.guilds.cache.get('774369837727350844').channels.cache.get(idDiscussione).members;
+
     const promiseCriminali = new Promise((resolve) => {
       var criminali = [];
       const ruoliCriminali = ["786684300175212595", "787045347167305768", "787045379551526923", "787045304687525888"]
@@ -22,10 +26,29 @@ module.exports = {
 
     promiseCriminali.then((data) => {
       var criminale = message.guild.members.cache.find(member => member._roles.includes("786684300175212595" || "787045347167305768" || "787045379551526923" || "787045304687525888"))
-      if(numeroCriminali == 1)
-        message.channel.send(`Il criminale in gioco è: \n**${data}**`)
-      else
-        message.channel.send(`I criminali in gioco sono: \n**${data.join('**, **')}**`)
+
+      canaliCriminali.forEach((canale) => {
+        var membriCanale = client.guilds.cache.get('774369837727350844').channels.cache.get(canale).members;
+        membriCanale.forEach((membro) => {
+          var controllo = 0;
+          ruoliEsclusi.forEach(ruolo => {
+            if(membro._roles.includes(ruolo)) {
+              controllo = 1;
+            }
+          })
+          if(controllo == 1) {
+            return;
+          }
+          else {
+            if(membro.user.username != null) {
+              if(numeroCriminali == 1)
+                client.channels.cache.get(canale).send(`Il criminale in gioco è: \n**${data}**`);
+              else
+                client.channels.cache.get(canale).send(`I criminali in gioco sono: \n**${data.join('**, **')}**`)
+            }
+          }
+        });
+      });
     });
   }
 }
--- commands/guardie.js
+++ commands/guardie.js
@@ -1,7 +1,5 @@
 const canaliGuardie = ["787044655744417802", "787044778385866832", "787044819834241095"]
-const idNarratore = "774699081620521000";
-const idMorto = "780154332934438942";
-const idBot = "787753336169299998";
+const ruoliEsclusi = ['774699081620521000', '780154332934438942', '787753336169299998']
 
 module.exports = {
   name: "g",
@@ -28,15 +26,28 @@ module.exports = {
 
     promiseGuardie.then((data) => {
       var guardia = message.guild.members.cache.find(member => member._roles.includes("787045210617282631" || "787045249939800125" || "787045249939800125"))
+
       canaliGuardie.forEach((canale) => {
-        console.log(client.guilds.cache.get('774369837727350844').channels.cache.get(canale).members.find(user => !user._roles.includes(idNarratore || idMorto || idBot)))
-        if(!client.guilds.cache.get('774369837727350844').channels.cache.get(canale).members.find(user => user._roles.includes(idNarratore || idMorto || idBot))) {
-          if(numeroGuardie == 1)
-            client.channels.cache.get(canale).send(`La guardia in gioco è: \n**${data}**`);
-          else
-            client.channels.cache.get(canale).send(`Le guardie in gioco sono: \n**${data.join('**, **')}**`)
-        }
-        //console.log(client.guilds.cache.get('774369837727350844').channels.cache.get(canale).members.size)
+        var membriCanale = client.guilds.cache.get('774369837727350844').channels.cache.get(canale).members;
+        membriCanale.forEach((membro) => {
+          var controllo = 0;
+          ruoliEsclusi.forEach(ruolo => {
+            if(membro._roles.includes(ruolo)) {
+              controllo = 1;
+            }
+          })
+          if(controllo == 1) {
+            return;
+          }
+          else {
+            if(membro.user.username != null) {
+              if(numeroGuardie == 1)
+                client.channels.cache.get(canale).send(`La guardia in gioco è: \n**${data}**`);
+              else
+                client.channels.cache.get(canale).send(`Le guardie in gioco sono: \n**${data.join('**, **')}**`)
+            }
+          }
+        });
       });
     });
   }
