7e4bd472a9f0eb4c8487c810bdbce4d55ed693aa
Aizakku2996 <jacopo_bordi@hotmail.it>
2021-01-25 20:41:57 +0100

    traditore

--- commands/notte.js
+++ commands/notte.js
@@ -1,6 +1,7 @@
 const idNarratore = "774699081620521000";
 const idDiscussione = '774710293363949618';
-
+const idFazioneOmbra = '774709180073771059';
+var turni = 0;
 
 const mute = (members) => {
   members.forEach(member => {
@@ -15,11 +16,36 @@ module.exports = {
   description: 'Comando per avviare la fase del gioco notte.',
 
   execute(client, message, args) {
-      message.channel.messages.fetch({ limit: 10 }).then(messages => {
-      message.channel.bulkDelete(messages);
+    members = client.guilds.cache.get('774369837727350844').channels.cache.get(idDiscussione).members;
+    const promiseTraditore = new Promise((resolve) => {
+      var traditore = [];
+      const idTraditore = "774684173787987978"
+      members.forEach((member) => {
+          if(member._roles.includes(idTraditore)) {
+            traditore.push(member.user.username)
+          }
       });
-      members = client.guilds.cache.get('774369837727350844').channels.cache.get(idDiscussione).members;
-      message.channel.send('bnotte guys');
-      mute(members)
+      resolve(traditore);
+    });
+    promiseTraditore.then((data) => {
+      if(turni == 0) {
+        message.channel.messages.fetch({ limit: 10 }).then(messages => {
+            message.channel.bulkDelete(messages);
+        });
+        client.channels.cache.get(idFazioneOmbra).send(`Il Traditore è **${data}**, ricordo che uccidendolo riconoscerà il branco ma quella notte non morirà nessuno.`)
+        members = client.guilds.cache.get('774369837727350844').channels.cache.get(idDiscussione).members;
+        message.channel.send('bnotte guys');
+        mute(members)
+      }
+      else {
+        message.channel.messages.fetch({ limit: 10 }).then(messages => {
+            message.channel.bulkDelete(messages);
+        });
+        members = client.guilds.cache.get('774369837727350844').channels.cache.get(idDiscussione).members;
+        message.channel.send('bnotte guys');
+        mute(members)
+      }
+      turni++
+    });
   }
 }
